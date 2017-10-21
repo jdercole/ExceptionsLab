61bac08bde5e0e7c8e78ecd56026b3c754b09580
Genna1 <Genna1@Jenna>
2017-10-21 10:40:14 -0500

    Completed extractLastName method, throws exception to calling method, input loops until valid data is received.

---
+++ src/lab2/InputOutputGui.java
@@ -0,0 +1,34 @@
+package lab2;
+
+import javax.swing.JOptionPane;
+
+/**
+ * This class is responsible for all input and output in the program.
+ * 
+ * @author  Jim Lombardo, jlombardo@wctc.edu
+ * @version 1.00
+ */
+public class InputOutputGui {
+    private NameService nameService;
+
+    public InputOutputGui() {
+        nameService = new NameService();
+    }
+
+    public final void startConversation() {
+        boolean isValid = false;
+        do {
+            try {
+                String fullName = JOptionPane.showInputDialog("Enter full name:");
+                String lastName = "";
+                lastName = nameService.extractLastName(fullName);
+                String msg = "Your last name is: " + lastName;
+                JOptionPane.showMessageDialog(null, msg);
+                isValid = true;
+            } catch (IllegalArgumentException iae) {
+                JOptionPane.showMessageDialog(null, iae.getMessage());
+            }
+        } while (!isValid);
+    }
+}
+
